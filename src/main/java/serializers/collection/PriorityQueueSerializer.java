b42362c12600f9098b440b6d55a476538aa94fcb
Fábio Baião <a75662@alunos.uminho.pt>
2018-06-14 23:34:02 +0100

    Agora sim, corrigido erro relacionado com tamanho da Queue

--- src/main/java/serializers/collection/PriorityQueueSerializer.java
+++ src/main/java/serializers/collection/PriorityQueueSerializer.java
@@ -6,6 +6,6 @@ public class PriorityQueueSerializer extends QueueSerializer<PriorityQueue<?>> {
 
     @Override
     protected PriorityQueue<?> createQueue(int size) {
-        return (size < 0 ? new PriorityQueue<>() : new PriorityQueue<>(size));
+        return (size <= 0 ? new PriorityQueue<>() : new PriorityQueue<>(size));
     }
 }
